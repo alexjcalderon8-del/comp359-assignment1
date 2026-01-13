b2136bb391a0fe08720d0d0b7c46434b5b489235
nishantsingh604 <nishantrohilla28@gmail.com>
2026-01-12 20:35:04 -0800

    started with the not in place mergesort, and drafted the structure for the pre-alloc mergesort

---
+++ not_in_place_nish/mergesort_preallocated_array.py
@@ -0,0 +1,27 @@
+#this is the type of not in place merge sort where we preallocate a temporary array for the mergesort 
+
+# immplementing - standard mergesort with preallocated memory
+
+import random
+import time
+from typing import List 
+#type hinting to type list
+
+class MergeSortPrealloc:
+    
+    def sort (): 
+        #method to sort an array
+        return 
+    
+    def mergesort():
+        #method to do mergesort recursively
+        pass
+    
+    def merge():
+        #method to merge two sorted halves
+        pass
+    
+    def test():
+        #method to test the implementation or the basic setup
+        pass
+    
